8ea4476ea5554b660bb32d1f11269b588ded1b68
Lucas Augusto <lucasaugustscode@gmail.com>
2025-01-24 19:55:27 -0300

    chore(repo): delete unnecessary dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
@@ -13,28 +12,5 @@ export default defineConfig({
         replacement: path.resolve(__dirname, 'packages/react/src'),
       },
     ],
-  },
-  test: {
-    include: ['./src/**/*.spec.{ts,tsx}'],
-    setupFiles: [path.resolve('../..', 'vitest.setup.ts')],
-    globals: true,
-    coverage: {
-      enabled: true,
-      reporter: 'html',
-      cleanOnRerun: true,
-      provider: 'v8',
-      reportsDirectory: path.resolve(__dirname, 'coverage'),
-      exclude: ['./__stories__/*'],
-    },
-    browser: {
-      enabled: true,
-      provider: 'playwright',
-      instances: [
-        {
-          browser: 'chromium',
-        },
-      ],
-      headless: false,
-    },
   },
 })
