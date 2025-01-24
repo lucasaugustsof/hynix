52cb0064588c69c3e257e55bcbd2b3c4d161486d
Lucas Augusto <lucasaugustscode@gmail.com>
2025-01-24 19:53:34 -0300

    feat(button): create button component

---
+++ packages/react/__stories__/components/buttons/button.stories.tsx
@@ -0,0 +1,168 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { expect, fn, userEvent, within } from '@storybook/test'
+
+import { Button, type ButtonProps } from '@/registry/components/buttons/button'
+
+import {
+  RiArrowLeftLine,
+  RiArrowRightLine,
+  RiGithubFill,
+} from '@remixicon/react'
+
+const meta: Meta<ButtonProps> = {
+  title: 'components/buttons/Button',
+  component: Button,
+  args: {
+    variant: 'primary',
+    size: 'md',
+    children: 'Button',
+    disabled: false,
+    iconOnly: false,
+    onClick: fn(),
+  },
+  argTypes: {
+    variant: {
+      control: 'inline-radio',
+      options: ['primary', 'secondary', 'ghost', 'destructive'],
+      description: 'The visual style variant of the button',
+    },
+    size: {
+      control: 'inline-radio',
+      options: ['sm', 'md', 'lg', 'xl'],
+      description: 'The size of the button',
+    },
+    disabled: {
+      description: 'Whether the button is disabled and non-interactive',
+    },
+    iconOnly: {
+      control: false,
+      description:
+        'Whether the button contains only an icon (requires aria-label)',
+    },
+    asChild: {
+      control: false,
+      description:
+        'Whether to merge props onto a child element instead of creating a new button element',
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement)
+
+    const sut = canvas.getByRole('button')
+
+    await userEvent.click(sut)
+    await userEvent.click(canvasElement)
+
+    await expect(args.onClick).toHaveBeenCalled()
+  },
+}
+
+export default meta
+
+export const Basic: StoryObj<ButtonProps> = {}
+
+export const WithPrefix: StoryObj<ButtonProps> = {
+  render({ ...args }) {
+    return (
+      <Button {...args}>
+        <RiArrowLeftLine />
+        {args.children}
+      </Button>
+    )
+  },
+}
+
+export const WithSuffix: StoryObj<ButtonProps> = {
+  render({ ...args }) {
+    return (
+      <Button {...args}>
+        {args.children}
+        <RiArrowRightLine />
+      </Button>
+    )
+  },
+}
+
+export const WithPrefixAndSuffix: StoryObj<ButtonProps> = {
+  render({ ...args }) {
+    return (
+      <Button {...args}>
+        <RiArrowLeftLine />
+        {args.children}
+        <RiArrowRightLine />
+      </Button>
+    )
+  },
+}
+
+export const IconOnly: StoryObj<ButtonProps> = {
+  args: {
+    iconOnly: true,
+    'aria-label': 'Go to GitHub',
+  },
+  argTypes: {
+    children: {
+      control: false,
+    },
+  },
+  render({ ...args }) {
+    return (
+      <Button {...args}>
+        <RiGithubFill />
+      </Button>
+    )
+  },
+}
+
+export const LinkBehavior: StoryObj<ButtonProps> = {
+  tags: ['skip-test'],
+  args: {
+    iconOnly: true,
+    'aria-label': 'Go to GitHub',
+    asChild: true,
+  },
+  argTypes: {
+    children: {
+      control: false,
+    },
+    disabled: {
+      control: false,
+    },
+  },
+  render({ ...args }) {
+    return (
+      <Button {...args} data-testid="github-link">
+        <a href="https://github.com/lucasaugustsof/hynix">
+          <RiGithubFill />
+        </a>
+      </Button>
+    )
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const sut = canvas.getByTestId('github-link')
+
+    await expect(sut.tagName.toLocaleLowerCase()).toEqual('a')
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+    onClick: fn(),
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement)
+
+    const sut = canvas.getByRole('button')
+
+    await userEvent.click(sut)
+    await userEvent.click(canvasElement)
+
+    await expect(args.onClick).not.toHaveBeenCalled()
+  },
+}
---
+++ packages/react/src/components/buttons/button.tsx
@@ -0,0 +1,101 @@
+import { ark } from '@ark-ui/react'
+import { type VariantProps, cva } from 'class-variance-authority'
+
+import { cn } from '@/registry/utils/cn'
+
+type ButtonProps = React.ComponentPropsWithRef<'button'> &
+  VariantProps<typeof buttonStyles> & {
+    asChild?: boolean
+  }
+
+const buttonStyles = cva(
+  [
+    'inline-flex items-center justify-center whitespace-nowrap rounded-3xl font-semibold transition-colors ease-in-out',
+    'focus-visible:outline-3 focus-visible:outline-brand-selected',
+  ],
+  {
+    variants: {
+      variant: {
+        primary: [
+          'inset-ring-brand bg-brand text-fg-2',
+          'enabled:hover:bg-brand-hover',
+        ],
+        secondary: [
+          'inset-ring-border bg-surface-2 text-fg-1',
+          'enabled:hover:bg-fill-2',
+        ],
+        ghost: [
+          'inset-ring-brand bg-transparent text-brand',
+          'enabled:hover:bg-brand/8',
+        ],
+        destructive: [
+          'inset-ring-0 bg-danger text-fg-2',
+          'enabled:hover:bg-danger-hover',
+        ],
+      },
+      size: {
+        sm: [
+          'h-9 min-h-9 gap-1 px-3 text-sm leading-snug',
+          'only:[&_svg]:size-5',
+        ],
+        md: ['h-11 min-h-11 gap-2 px-4 text-base', 'only:[&_svg]:size-6'],
+        lg: [
+          'h-11 min-h-11 gap-3 px-4 text-lg leading-7',
+          'only:[&_svg]:size-7',
+        ],
+        xl: [
+          'h-14 min-h-14 gap-3 px-6 text-xl leading-8',
+          'only:[&_svg]:size-8',
+        ],
+      },
+      disabled: {
+        true: 'cursor-not-allowed bg-fill-1 text-disabled',
+        false: 'inset-ring-1 cursor-pointer',
+      },
+      iconOnly: {
+        true: 'aspect-square px-0',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
+      disabled: false,
+      iconOnly: false,
+    },
+  },
+)
+
+function Button({
+  className,
+  variant,
+  size,
+  disabled,
+  iconOnly,
+  ...props
+}: ButtonProps) {
+  if (iconOnly && !props['aria-label']) {
+    console.warn(
+      'Button: `aria-label` prop is required when `iconOnly` is true',
+    )
+  }
+
+  return (
+    <ark.button
+      {...props}
+      className={cn(
+        buttonStyles({
+          className,
+          variant,
+          size,
+          iconOnly,
+          disabled,
+        }),
+      )}
+      disabled={disabled}
+      tabIndex={disabled ? -1 : 0}
+    />
+  )
+}
+
+export { Button, buttonStyles }
+export type { ButtonProps }
