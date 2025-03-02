eb2f59057a82d3f3df04ea497d51aded64fbe13d
Lucas Augusto <lucasaugustscode@gmail.com>
2025-03-01 21:53:48 -0300

    refactor(input): add ark ui component input

--- packages/react/src/components/input.tsx
+++ packages/react/src/components/input.tsx
@@ -1,13 +1,12 @@
 // Hynix: Input [v1.0.0]
 
-import { Input as BaseInput } from '@base-ui-components/react/input'
-
+import { Field as ArkField } from '@ark-ui/react/field'
 import { type VariantProps, cva } from 'class-variance-authority'
 
 import { cn } from '@/registry/utils/cn'
 
 export type InputProps = Omit<
-  React.ComponentPropsWithRef<typeof BaseInput>,
+  React.ComponentPropsWithRef<typeof ArkField.Input>,
   'size'
 > &
   VariantProps<typeof inputStyles>
@@ -15,9 +14,14 @@ export type InputProps = Omit<
 const inputStyles = cva(
   [
     'inline-flex min-w-60 shrink-0 cursor-text overflow-hidden rounded-3xl border border-border bg-surface-1 font-medium font-sans caret-brand outline-none ring-offset-2 ring-offset-surface-1 transition-[colors_shadow] ease-out',
+    // focus-visible
     'focus-visible:border-brand focus-visible:ring-2 focus-visible:ring-brand-selected/40',
+    // invalid
     'data-invalid:border-danger data-invalid:ring-2 data-invalid:ring-danger-selected/40 data-invalid:hover:bg-surface-1',
-    'placeholder:text-fg-1/70',
+    // disabled
+    'not-disabled:not-focus-visible:hover:bg-fill-1 disabled:cursor-not-allowed disabled:border-transparent disabled:bg-fill-1 disabled:text-disabled',
+    // placeholder
+    'placeholder:text-fg-1/70 disabled:placeholder:text-disabled',
   ],
   {
     variants: {
@@ -26,37 +30,23 @@ const inputStyles = cva(
         md: ['h-11 pr-3 pl-4 text-base'],
         lg: ['h-14 pr-5 pl-5 text-lg leading-7'],
       },
-      disabled: {
-        true: 'cursor-not-allowed border-transparent bg-fill-1 text-disabled placeholder:text-disabled',
-      },
     },
-    compoundVariants: [
-      {
-        disabled: false,
-        class: ['not-focus-visible:hover:bg-fill-1'],
-      },
-    ],
     defaultVariants: {
       size: 'md',
-      disabled: false,
     },
   },
 )
 
-export function Input({ className, size, disabled, ...props }: InputProps) {
+export function Input({ className, size, ...props }: InputProps) {
   return (
-    <BaseInput
+    <ArkField.Input
       {...props}
       className={cn(
         inputStyles({
           className,
           size,
-          disabled,
         }),
       )}
-      disabled={disabled}
-      tabIndex={disabled ? -1 : 0}
-      aria-disabled={disabled}
     />
   )
 }
