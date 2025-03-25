23af12432cdd1b31f66022645d9aa3ac17adc070
Lucas Augusto <lucasaugustscode@gmail.com>
2025-03-25 00:15:01 -0300

    feat(divider): create Divider component

---
+++ packages/react/__stories__/components/divider.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Divider, type DividerProps } from 'registry/components/divider'
+
+type DividerStory = StoryObj<DividerProps>
+
+const meta: Meta<DividerProps> = {
+  title: 'components/Divider',
+  component: Divider,
+  args: {
+    type: 'default',
+    labelText: 'Label',
+    direction: 'horizontal',
+  },
+  argTypes: {
+    type: {
+      control: false,
+      table: {
+        disable: true,
+      },
+    },
+    labelText: {
+      name: 'Label Text',
+      control: 'text',
+      description:
+        'Text that will be used as the label within the divider when using label variations.',
+      table: {
+        category: 'Content',
+      },
+    },
+    direction: {
+      control: 'inline-radio',
+      options: ['vertical', 'horizontal'],
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['new'],
+}
+
+export const Default: DividerStory = {}
+
+export const LabelLeft: DividerStory = {
+  render(args) {
+    return <Divider {...args} type="label-left" />
+  },
+}
+
+export const LabelCenter: DividerStory = {
+  render(args) {
+    return <Divider {...args} type="label-center" />
+  },
+}
+
+export const LabelRight: DividerStory = {
+  render(args) {
+    return <Divider {...args} type="label-right" />
+  },
+}
+
+export default meta
