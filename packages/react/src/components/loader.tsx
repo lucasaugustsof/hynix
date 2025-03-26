2fca3c6296687b1fd69a427690d4557d682a37c0
Lucas Augusto <lucasaugustscode@gmail.com>
2025-03-26 09:47:50 -0300

    feat(loader): create Loader component

---
+++ packages/react/src/components/loader.tsx
@@ -0,0 +1,38 @@
+import { type VariantProps, tv } from 'registry/utilities/tv'
+import { cn } from 'registry/utilities/cn'
+
+type LoaderProps = VariantProps<typeof loaderVariants>
+
+const loaderVariants = tv({
+  base: [
+    'block animate-spin rounded-full bg-conic from-transparent to-brand',
+    '[mask-image:radial-gradient(circle_at_center,transparent_60%,black_0%)]',
+  ],
+  variants: {
+    size: {
+      sm: 'size-8',
+      md: 'size-11',
+      lg: 'size-14.5',
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+function Loader({ size }: LoaderProps) {
+  return (
+    <div
+      role="status"
+      className={cn(
+        loaderVariants({
+          size,
+        }),
+      )}
+      data-scope="loader"
+    />
+  )
+}
+
+export { Loader }
+export type { LoaderProps }
