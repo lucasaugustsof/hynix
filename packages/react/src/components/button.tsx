87a467f749b767817aa3b1e9c35d6a2f16eca7a1
Lucas Augusto <lucasaugustscode@gmail.com>
2025-04-08 18:20:41 -0300

    refactor(button): removing motion usage

--- packages/react/src/components/button.tsx
+++ packages/react/src/components/button.tsx
@@ -1,8 +1,6 @@
 import { type Assign, ark } from '@ark-ui/react'
-import { type Transition, motion, useReducedMotion } from 'motion/react'
 
 import { cn } from 'registry/utilities/cn'
-import { focusEffect } from 'registry/utilities/focus-effect'
 import { type VariantProps, tv } from 'registry/utilities/tv'
 
 type ButtonProps = Assign<
@@ -10,15 +8,12 @@ type ButtonProps = Assign<
   VariantProps<typeof buttonVariants>
 >
 
-const ButtonComponent = motion.create(ark.button)
-
 const buttonVariants = tv({
   base: [
     [
-      'not-disabled:inset-ring-1 inline-flex cursor-pointer select-none items-center justify-center whitespace-nowrap rounded-xl transition-colors ease-out',
-      'font-sans font-semibold tracking-normal',
-      'disabled:cursor-not-allowed disabled:bg-fill-1 disabled:text-disabled',
-      focusEffect,
+      'inline-flex cursor-pointer select-none items-center justify-center whitespace-nowrap rounded-xl font-sans font-semibold tracking-normal',
+      'not-disabled:inset-ring-1 disabled:cursor-not-allowed disabled:bg-fill-1 disabled:text-disabled',
+      'transition-[background-color,scale] ease-in-out-quad active:scale-95',
     ],
   ],
   variants: {
@@ -38,10 +33,10 @@ const buttonVariants = tv({
       ],
     },
     size: {
-      sm: 'h-9 gap-1 px-3 text-sm leading-5.5 [&_svg]:size-5',
+      sm: 'h-9 gap-1 px-3 text-sm/5.5 [&_svg]:size-5',
       md: 'h-11 gap-2 px-4 text-base [&_svg]:size-6',
-      lg: 'h-11 px-4 text-lg leading-7 [&_svg]:size-7',
-      xl: 'h-14 px-6 text-xl leading-8 [&_svg]:size-8',
+      lg: 'h-11 px-4 text-lg/7 [&_svg]:size-7',
+      xl: 'h-14 px-6 text-xl/8 [&_svg]:size-8',
     },
     iconOnly: {
       true: 'aspect-square px-0',
@@ -64,22 +59,9 @@ const buttonVariants = tv({
   },
 })
 
-const MOTION_TRANSITION = {
-  type: 'spring',
-  bounce: 0,
-  duration: 0.4,
-} satisfies Transition
-
 function Button({ variant, size, iconOnly, ...props }: ButtonProps) {
-  const shouldReduceMotion = useReducedMotion()
-
   return (
-    /**
-     * @NOTE: This approach was adopted to work around an incompatibility issue in prop merging
-     * between Ark UI and Motion.
-     */
-    // @ts-ignore
-    <ButtonComponent
+    <ark.button
       {...props}
       className={cn(
         buttonVariants({
@@ -88,15 +70,8 @@ function Button({ variant, size, iconOnly, ...props }: ButtonProps) {
           iconOnly,
         }),
       )}
-      whileTap={
-        shouldReduceMotion
-          ? undefined
-          : {
-              scale: 0.98,
-            }
-      }
-      transition={MOTION_TRANSITION}
       data-scope="button"
+      data-focusable
     />
   )
 }
