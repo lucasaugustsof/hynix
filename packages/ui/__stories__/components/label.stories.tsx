c6a2450b62d2b4e8606e06491375ee965d9e73c5
Lucas Augusto <lucasaugustscode@gmail.com>
2025-06-22 13:53:54 -0300

    refactor(label): simplifying label component

--- packages/ui/__stories__/components/label.stories.tsx
+++ packages/ui/__stories__/components/label.stories.tsx
@@ -11,7 +11,6 @@ const meta: Meta<LabelStoryArgs> = {
   tags: ['autodocs', 'code-only'],
   args: {
     children: 'Label text',
-    size: 'md',
   },
   argTypes: {
     children: {
@@ -22,16 +21,6 @@ const meta: Meta<LabelStoryArgs> = {
       control: {
         type: 'text',
       },
-    },
-    size: {
-      description: 'Controls the font size of the label.',
-      table: {
-        category: 'Style',
-      },
-      control: {
-        type: 'inline-radio',
-      },
-      options: ['sm', 'md', 'lg', 'xl'],
     },
   },
 }
@@ -51,43 +40,10 @@ export const Basic: LabelStory = {
   },
 }
 
-export const FormFieldLabel: LabelStory = {
-  name: 'Form field label',
-  args: {
-    children: 'Email address',
-    size: 'sm',
-  },
-  parameters: {
-    docs: {
-      description: {
-        story:
-          'Use a small label to annotate form input fields like text or email.',
-      },
-    },
-  },
-}
-
-export const SectionTitle: LabelStory = {
-  name: 'Section title',
-  args: {
-    children: 'Billing information',
-    size: 'xl',
-  },
-  parameters: {
-    docs: {
-      description: {
-        story:
-          'Use a large label to denote section headings in a layout or form.',
-      },
-    },
-  },
-}
-
 export const DisabledLabel: LabelStory = {
   name: 'Disabled label',
   args: {
     children: 'Username (disabled)',
-    size: 'md',
     'data-disabled': true,
   },
   parameters: {
--- packages/ui/src/components/label.tsx
+++ packages/ui/src/components/label.tsx
@@ -1,43 +1,21 @@
-import { type Assign, ark } from '@ark-ui/react'
+import { ark } from '@ark-ui/react'
 
 import { cn } from '@r/utilities/cn'
-import { type VariantProps, tv } from '@r/utilities/tv'
 
-const labelVariants = tv({
-  base: [
-    'text-nowrap font-medium font-sans text-fg-1 tracking-normal',
-    'data-disabled:cursor-not-allowed data-disabled:text-disabled',
-  ],
-  variants: {
-    size: {
-      sm: 'text-sm/5.5',
-      md: 'text-base',
-      lg: 'text-lg/7',
-      xl: 'text-xl/8',
-    },
-  },
-  defaultVariants: {
-    size: 'md',
-  },
-})
+type LabelProps = React.ComponentProps<typeof ark.span>
 
-export type LabelProps = Assign<
-  React.CustomComponentPropsWithRef<typeof ark.span>,
-  VariantProps<typeof labelVariants>
->
-
-export function Label({ className, size, ...props }: LabelProps) {
+const Label = ({ className, ...props }: LabelProps) => {
   return (
     <ark.span
       {...props}
       className={cn(
-        labelVariants({
-          className,
-          size,
-        }),
+        'text-nowrap font-medium text-base text-fg-1 tracking-normal',
+        'data-disabled:cursor-not-allowed data-disabled:text-disabled',
       )}
     />
   )
 }
 
 Label.displayName = 'Label'
+
+export { Label, type LabelProps }
