e0577cbcf349ef67c91225a3df03c28e152ab5be
Lucas Augusto <lucasaugustscode@gmail.com>
2025-10-12 19:46:21 -0300

    feat(button): story with animation

--- packages/react/src/__stories__/button.stories.tsx
+++ packages/react/src/__stories__/button.stories.tsx
@@ -1,7 +1,11 @@
-import { RiAddLine, RiArrowLeftSLine, RiArrowRightSLine, RiDeleteBinLine } from '@remixicon/react'
+import React from 'react'
+
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
+import { ChevronLeft, ChevronRight, CircleCheck, Copy, Plus, Trash } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
 import { Button, type ButtonProps } from '@/components/button'
+import { cn } from '@/lib/cn'
 
 export default {
   title: 'Components/Button',
@@ -92,34 +96,36 @@ export default {
   },
 } satisfies Meta<ButtonProps>
 
-export const Default: StoryObj<ButtonProps> = {
+type ButtonStory = StoryObj<ButtonProps>
+
+export const Default: ButtonStory = {
   args: {
     children: 'Button',
   },
 }
 
-export const AllVariants: StoryObj<ButtonProps> = {
+export const AllVariants: ButtonStory = {
   render: () => (
     <div className="flex flex-wrap items-center gap-6">
       <Button variant="primary">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
       <Button variant="secondary">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
       <Button variant="outline">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
       <Button variant="danger">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
     </div>
   ),
@@ -130,28 +136,28 @@ export const AllVariants: StoryObj<ButtonProps> = {
   },
 }
 
-export const AllSizes: StoryObj<ButtonProps> = {
+export const AllSizes: ButtonStory = {
   render: () => (
     <div className="flex flex-wrap items-center gap-6">
       <Button size="2xs">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
       <Button size="xs">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
       <Button size="sm">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
       <Button size="md">
-        <RiArrowLeftSLine />
+        <ChevronLeft />
         Button
-        <RiArrowRightSLine />
+        <ChevronRight />
       </Button>
     </div>
   ),
@@ -162,23 +168,86 @@ export const AllSizes: StoryObj<ButtonProps> = {
   },
 }
 
-export const IconOnly: StoryObj<ButtonProps> = {
+export const IconOnly: ButtonStory = {
   render: () => (
     <div className="flex flex-wrap items-center gap-6">
       <Button iconOnly variant="primary">
-        <RiAddLine />
+        <Plus />
       </Button>
       <Button iconOnly variant="secondary">
-        <RiAddLine />
+        <Plus />
       </Button>
       <Button iconOnly variant="outline">
-        <RiAddLine />
+        <Plus />
       </Button>
       <Button iconOnly variant="danger">
-        <RiDeleteBinLine />
+        <Trash />
       </Button>
     </div>
   ),
+  parameters: {
+    controls: {
+      disable: true,
+    },
+  },
+}
+
+export const WithAnimation: ButtonStory = {
+  render() {
+    function CopyButton() {
+      const [isCopied, setIsCopied] = React.useState(false)
+
+      function handleCopyText() {
+        const TIMEOUT = 2000
+
+        navigator.clipboard.writeText('Hello World')
+        setIsCopied(true)
+
+        setTimeout(() => setIsCopied(false), TIMEOUT)
+      }
+
+      return (
+        <Button
+          onClick={handleCopyText}
+          className={cn(
+            isCopied && 'pointer-events-none',
+            'transition-[background-color,box-shadow,scale] active:scale-98'
+          )}
+          variant="secondary"
+        >
+          <AnimatePresence initial={false} mode="popLayout">
+            <motion.span
+              key={isCopied ? 'copied' : 'copy'}
+              initial={{
+                opacity: 0.5,
+                filter: 'blur(2px)',
+                scale: 0.9,
+              }}
+              animate={{
+                opacity: 1,
+                filter: 'blur(0px)',
+                scale: 1,
+              }}
+              exit={{
+                opacity: 0,
+                filter: 'blur(2px)',
+                scale: 0.9,
+              }}
+              transition={{
+                ease: 'easeOut',
+                duration: 0.15,
+              }}
+            >
+              {isCopied ? <CircleCheck /> : <Copy />}
+            </motion.span>
+          </AnimatePresence>
+          Copy
+        </Button>
+      )
+    }
+
+    return <CopyButton />
+  },
   parameters: {
     controls: {
       disable: true,
