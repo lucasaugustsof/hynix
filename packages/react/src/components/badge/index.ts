8e2f48d6bfe1ba6903242b6e5a0c337a1ca9f41f
Lucas Augusto <lucasaugustscode@gmail.com>
2025-10-20 19:23:29 -0300

    chore(badge): remove comment

--- packages/react/src/components/badge/index.ts
+++ packages/react/src/components/badge/index.ts
@@ -1,14 +1,2 @@
 export type { BadgeRootProps } from './badge'
 export * as Badge from './namespace'
-
-/**
- * <Badge.Root>
- *  <Badge.Icon />
- *  Badge
- * </Badge.Root>
- *
- * <Badge.Root>
- *  <Badge.Dot />
- *  Badge
- * </Badge.Root>
- */
