281775107161130f4795a3839a54ec79aa33989d
Lucas Augusto <lucasaugustscode@gmail.com>
2025-10-25 18:54:07 -0300

    feat(switch): create Switch component

---
+++ packages/react/src/components/switch/index.ts
@@ -0,0 +1,5 @@
+export type { UseSwitchContext, UseSwitchProps, UseSwitchReturn } from '@ark-ui/react/switch'
+export { useSwitch, useSwitchContext } from '@ark-ui/react/switch'
+
+export * as Switch from './namespace'
+export type { SwitchControlProps, SwitchRootProps } from './switch'
