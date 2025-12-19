a79d3ab79e2457303591161848cc9ccd2ccd0445
Lucas Augusto <lucasaugustscode@gmail.com>
2025-12-19 12:53:48 -0300

    feat(storybook): define custom theme in storybook

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from 'storybook/theming'
+
+export default create({
+  base: 'dark',
+  brandImage: '/assets/hynix-white.svg',
+  brandTitle: 'Hynix Storybook',
+  brandUrl: 'https://hynix.cc',
+  brandTarget: '_self',
+})
