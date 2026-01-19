c54ca723884e785169bad8286a570bd228fbc5bc
Lucas Augusto <lucasaugustscode@gmail.com>
2026-01-19 12:45:46 -0300

    feat(react): add tokens shadow

--- packages/react/src/__stories__/hello.stories.tsx
+++ packages/react/src/__stories__/hello.stories.tsx
@@ -2,9 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 
 export default {
   title: 'components/Hello',
-  component: () => (
-    <h1 className="font-sans text-label-xl">The quick brown fox jumps over the lazy dog.</h1>
-  ),
+  component: () => <h1>Hello World</h1>,
 } satisfies Meta
 
 export const Default: StoryObj = {}
